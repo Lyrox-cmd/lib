9c6861d2974094809748538f70c7684302e904d1
Lyrox-cmd <tyrafi30@gmail.com>
2026-07-16 23:24:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Lyrox",
-    version="1.5.0",
+    version="1.5.1",
     py_modules=["Lyrox"],
     install_requires=["base91"],
     author="Lyrox",
