f7442b9c7abb88e19d46c3f526e717c7ad7b93d5
Lyrox-cmd <tyrafi30@gmail.com>
2026-07-26 11:55:47 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="Lyrox",
-    version="1.5.1",
+    version="2.5",
     py_modules=["Lyrox"],
-    install_requires=["base91"],
     author="Lyrox",
-    description="Telegram; @LyroxPy",
+    description="Telegram: @LyroxPy",
+    url="https://t.me/LyroxPy",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
